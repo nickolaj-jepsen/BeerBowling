b2982af850c000bcfd7d9f598b0796d57208f2ef
Nickolaj Jepsen <nij@billigvvs.dk>
2020-06-01 17:54:12 +0200

    Fix dev db location

--- beerbowling/settings/development.py
+++ beerbowling/settings/development.py
@@ -15,6 +15,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     }
 }
